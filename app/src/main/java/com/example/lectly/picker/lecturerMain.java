b087af1eca02ee953459655bacc2ebe9ea2b83a9
Amy Wood <a.wood12@rgu.ac.uk>
2021-02-24 12:07:57 +0000

    adding create post dialog box

--- app/src/main/java/com/example/lectly/picker/lecturerMain.java
+++ app/src/main/java/com/example/lectly/picker/lecturerMain.java
@@ -2,16 +2,20 @@ package com.example.lectly.picker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.lectly.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class lecturerMain extends AppCompatActivity {
 
     Button files;
+    FloatingActionButton createButton;
     Button postButton;
     Button menu;
 
@@ -27,6 +31,7 @@ public class lecturerMain extends AppCompatActivity {
         files = findViewById(R.id.files);
         postButton = findViewById(R.id.postButton);
         menu = findViewById(R.id.menu);
+        createButton = findViewById(R.id.createButton);
     }
 
     private void setupListeners() {
@@ -37,7 +42,40 @@ public class lecturerMain extends AppCompatActivity {
                 mPicker.startPicking(lecturerMain.this, LinkType.DownloadLink);
             }
         });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder build = new AlertDialog.Builder(lecturerMain.this);
+
+                build.setTitle("Create a Post");
+
+
+                //title of post
+
+
+                //description
+
+                //demonstration
+
+                //student work
+
+                //allow comments
+
+                //post in modules - drop down box to select modules
+
+                //schedule post button
+
+                //post now button
+
+                //help button - explain concept of contructivist learning theory
+                AlertDialog dialog = build.create();
+                dialog.show();
+            }
+        });
     }
+
+
 }
 
 
