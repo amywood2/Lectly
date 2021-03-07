9d138e584a1cd7ef1c5d3fc866f22bd724141501
amywood2 <a.wood12@rgu.ac.uk>
2021-03-07 23:15:06 +0000

    working on implementing pusher

--- app/src/main/java/com/example/lectly/lecturerMain.java
+++ app/src/main/java/com/example/lectly/lecturerMain.java
@@ -2,6 +2,8 @@ package com.example.lectly;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -24,8 +26,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
+import com.pusher.client.Pusher;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.SubscriptionEventListener;
+import com.pusher.client.connection.ConnectionEventListener;
+import com.pusher.client.connection.ConnectionStateChange;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -34,13 +44,18 @@ public class lecturerMain extends AppCompatActivity {
     private static final String TAG = null;
     Button files;
     FloatingActionButton createButton;
-    Button postButton;
     Button menu;
     EditText postTitleInput;
     EditText postDescriptionInput;
     EditText postDemoInput;
     EditText postStudentWorkInput;
     Switch postAllowCommentsDecision;
+
+    //pusher
+    private RecyclerView.LayoutManager lManager;
+    private EventAdapter adapter;
+    private Pusher pusher = new Pusher("1b0541bc439a8001f9a6");
+    private static final String CHANNEL_NAME = "events_to_be_shown";
 
 
     @Override
@@ -49,11 +64,63 @@ public class lecturerMain extends AppCompatActivity {
         setContentView(R.layout.activity_lecturer_main);
         setupUI();
         setupListeners();
+
+        //pusher
+        // Get the RecyclerView
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // Use LinearLayout as the layout manager
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+
+        // Set the custom adapter
+        List<Event> eventList = new ArrayList<>();
+        adapter = new EventAdapter(eventList);
+        recycler.setAdapter(adapter);
+
+        Channel channel = pusher.subscribe(CHANNEL_NAME);
+
+        SubscriptionEventListener eventListener = new SubscriptionEventListener() {
+            @Override
+            public void onEvent(String channel, final String event, final String data) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Received event with data: " + data);
+                        Gson gson = new Gson();
+                        Event evt = gson.fromJson(data, Event.class);
+                        evt.setName(event + ":");
+                        adapter.addEvent(evt);
+                        ((LinearLayoutManager)lManager).scrollToPositionWithOffset(0, 0);
+                    }
+                });
+            }
+        };
+
+        channel.bind("created", eventListener);
+        channel.bind("updated", eventListener);
+        channel.bind("deleted", eventListener);
+
+        pusher.connect();
+
+        pusher.connect(new ConnectionEventListener() {
+            @Override
+            public void onConnectionStateChange(ConnectionStateChange change) {
+                System.out.println("State changed to " + change.getCurrentState() +
+                        " from " + change.getPreviousState());
+            }
+
+            @Override
+            public void onError(String message, String code, Exception e) {
+                System.out.println("There was a problem connecting!");
+                e.printStackTrace();
+            }
+        });
+
     }
 
     private void setupUI() {
         files = (Button) findViewById(R.id.files);
-        postButton = (Button) findViewById(R.id.postButton);
         menu = (Button) findViewById(R.id.menu);
         createButton = (FloatingActionButton) findViewById(R.id.createButton);
     }
@@ -125,6 +192,12 @@ public class lecturerMain extends AppCompatActivity {
         });
 
     }
+
+    public void onDestroy() {
+        super.onDestroy();
+        pusher.disconnect();
+    }
+
 
 }
 
